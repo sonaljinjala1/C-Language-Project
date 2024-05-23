a7b87818d518f33e5ccd08251b6783029c66eaf2
Sonal Jinjala <139161113+sonaljinjala1@users.noreply.github.com>
2024-05-23 16:42:00 +0530

    Add files via upload

---
+++ ch-1/Q-1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	clrscr();
+
+	printf("Name\t: Sonal Jinjala\n");
+	printf("Age\t: 21\n");
+	printf("School\t: Guatami Kanya Vidhyalay\n");
+
+	getch();
+}
